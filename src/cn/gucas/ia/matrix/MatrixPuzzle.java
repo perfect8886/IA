400c257221cdc921e9ec46241d79de302b024167
Perfect <perfect8886@hotmail.com>
2012-11-09 10:23:09 +0800

    optimization
    1. add a branches cutting condition
    2. add some annotations
    3. modify some variables' names
    4. modify the hashcode computation method

--- src/cn/gucas/ia/matrix/MatrixPuzzle.java
+++ src/cn/gucas/ia/matrix/MatrixPuzzle.java
@@ -5,24 +5,28 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-
-import cn.gucas.ia.structure.Stack;
+import java.util.Stack;
 
 /**
- * @Description: the answer to http://weibo.com/1915548291/z4eTPtAnv
+ * @Description: 
+ * 	the answer to http://weibo.com/1915548291/z4eTPtAnv 
+ * 		1. create 8 nodes; 
+ * 		2. use adjacent list to represent the graph's edges 
+ * 		3. breadth first search
  * @author pengf
  * @date 2012-11-8
  */
 
 public class MatrixPuzzle {
-	// define operation
-	private enum OPERATION {
+	/* 1. create nodes */
+	// 1.1 define operation
+	private enum Operation {
 		PLUS("+", 7), MINIUS("-", 5), TIMES("*", 3), DIVIDE("/", 2);
 
 		private final int value;
 		private final String op;
 
-		OPERATION(String op, int value) {
+		Operation(String op, int value) {
 			this.op = op;
 			this.value = value;
 		}
@@ -47,74 +51,86 @@ public class MatrixPuzzle {
 		}
 	}
 
-	// define direction
-	private enum DIRECTION {
-		LEFT, RIGHT;
+	// 1.2 define direction
+	private enum Direction {
+		LEFT("<-"), RIGHT("->");
+
+		private final String symbol;
+
+		Direction(String symbol) {
+			this.symbol = symbol;
+		}
+
+		@Override
+		public String toString() {
+			return symbol;
+		}
 	}
 
-	// declare node's structure
+	// 1.3 declare node's structure
 	private class Node {
-		OPERATION operation;
-		DIRECTION direction;
-		List<Node> nextStates;
+		Operation operation;
+		Direction direction;
+		List<Node> adjacentNodes;
 
-		Node(OPERATION operation, DIRECTION direction) {
+		Node(Operation operation, Direction direction) {
 			this.operation = operation;
 			this.direction = direction;
-			this.nextStates = new ArrayList<Node>();
+			this.adjacentNodes = new ArrayList<Node>();
 		}
 
-		void addNextState(Node s) {
-			nextStates.add(s);
+		void addAdjacentNode(Node s) {
+			adjacentNodes.add(s);
 		}
 	}
 
-	// define node in graph
-	Node plusLeft = new Node(OPERATION.PLUS, DIRECTION.LEFT);
-	Node plusRight = new Node(OPERATION.PLUS, DIRECTION.RIGHT);
-	Node minusLeft = new Node(OPERATION.MINIUS, DIRECTION.LEFT);
-	Node minusRight = new Node(OPERATION.MINIUS, DIRECTION.RIGHT);
-	Node timesLeft = new Node(OPERATION.TIMES, DIRECTION.LEFT);
-	Node timesRight = new Node(OPERATION.TIMES, DIRECTION.RIGHT);
-	Node divideLeft = new Node(OPERATION.DIVIDE, DIRECTION.LEFT);
-	Node divideRight = new Node(OPERATION.DIVIDE, DIRECTION.RIGHT);
+	// 1.4 define node in graph
+	Node plus7Left = new Node(Operation.PLUS, Direction.LEFT);
+	Node plus7Right = new Node(Operation.PLUS, Direction.RIGHT);
+	Node minus5Left = new Node(Operation.MINIUS, Direction.LEFT);
+	Node minus5Right = new Node(Operation.MINIUS, Direction.RIGHT);
+	Node times3Left = new Node(Operation.TIMES, Direction.LEFT);
+	Node times3Right = new Node(Operation.TIMES, Direction.RIGHT);
+	Node divide2Left = new Node(Operation.DIVIDE, Direction.LEFT);
+	Node divide2Right = new Node(Operation.DIVIDE, Direction.RIGHT);
 
-	// define edge in graph
+	/* 2. define edges in graph */
 	void initGraph() {
 		// plus left
-		plusLeft.addNextState(divideRight);
+		plus7Left.addAdjacentNode(divide2Right);
 
 		// plus right
-		plusRight.addNextState(timesRight);
-		plusRight.addNextState(minusRight);
-		plusRight.addNextState(divideLeft);
+		plus7Right.addAdjacentNode(times3Right);
+		plus7Right.addAdjacentNode(minus5Right);
+		plus7Right.addAdjacentNode(divide2Left);
 
 		// minus left
-		minusLeft.addNextState(timesRight);
-		minusLeft.addNextState(divideLeft);
-		minusLeft.addNextState(plusLeft);
+		minus5Left.addAdjacentNode(times3Right);
+		minus5Left.addAdjacentNode(divide2Left);
+		minus5Left.addAdjacentNode(plus7Left);
 
 		// minus right
-		minusRight.addNextState(timesLeft);
+		minus5Right.addAdjacentNode(times3Left);
 
 		// times left
-		timesLeft.addNextState(plusLeft);
-		timesLeft.addNextState(divideLeft);
-		timesLeft.addNextState(minusRight);
+		times3Left.addAdjacentNode(plus7Left);
+		times3Left.addAdjacentNode(divide2Left);
+		times3Left.addAdjacentNode(minus5Right);
 
 		// times right
-		timesRight.addNextState(minusLeft);
+		times3Right.addAdjacentNode(minus5Left);
 
 		// divide left
-		divideLeft.addNextState(plusRight);
+		divide2Left.addAdjacentNode(plus7Right);
 
 		// divide right
-		divideRight.addNextState(minusRight);
-		divideRight.addNextState(timesRight);
-		divideRight.addNextState(plusLeft);
+		divide2Right.addAdjacentNode(minus5Right);
+		divide2Right.addAdjacentNode(times3Right);
+		divide2Right.addAdjacentNode(plus7Left);
 	}
 
-	// declare statement
+	/* 3 search process */
+	// 3.1 declare state's structure, used to record the visited statement
 	private class State {
 		double input;
 		Node node;
@@ -135,56 +151,63 @@ public class MatrixPuzzle {
 		@Override
 		public int hashCode() {
 			int result = 17;
+			long f = Double.doubleToLongBits(input);
+			f = f ^ (f >> 32);
+			result = 31 * result + (int) f;
 			result = 31 * result + node.operation.value;
-			result = (int) (31 * result + input * 10);
 			return result;
 		}
 	}
 
-	// breadth first path function
+	// 3.2 breadth first search
 	private void bfs() {
 		// init
+		State startState = new State(2011, plus7Right, null);
+		HashSet<State> visitedStates = new HashSet<State>();
 		Queue<State> queue = new LinkedList<State>();
-		State startState1 = new State(2011, plusRight, null);
-		State startState2 = new State(2011, divideRight, null);
-		queue.add(startState1);
-		queue.add(startState2);
-		HashSet<State> states = new HashSet<State>();
-		states.add(startState1);
-		states.add(startState2);
+		queue.add(startState);
+		visitedStates.add(startState);
 
-		// bfs
+		// run
 		while (!queue.isEmpty()) {
 			State v = queue.poll();
 			double result = v.node.operation.apply(v.input);
-			if (result == 2012
-					&& (v.node.equals(timesRight) || v.node.equals(minusRight))) {
+			if (result == 2012 && (v.node.equals(minus5Right))) {
 				// show the path
-				Stack<State> stack = new Stack<State>();
-				while (v != null) {
-					stack.push(v);
-					v = v.prev;
-				}
-				for (State state : stack) {
-					System.out.println(state.input + " " + state.node.operation
-							+ "(" + state.node.direction + ")" + " =");
-				}
+				showPath(v);
 				return;
 			}
-
-			for (Node w : v.node.nextStates) {
+			// cut branches
+			if (result - (int) result > 0.0) {
+				continue;
+			}
+			for (Node w : v.node.adjacentNodes) {
 				State s = new State(result, w, v);
-				if (!states.contains(s)) {
-					states.add(s);
+				if (!visitedStates.contains(s)) {
+					visitedStates.add(s);
 					queue.add(s);
 				}
 			}
 		}
 	}
 
+	// 3.3 show path
+	private void showPath(State s) {
+		Stack<State> path = new Stack<State>();
+		while (s != null) {
+			path.add(s);
+			s = s.prev;
+		}
+		while (!path.isEmpty()) {
+			s = path.pop();
+			System.out.println(s.input + " " + s.node.operation + "("
+					+ s.node.direction + ")" + " =");
+		}
+	}
+
 	public static void main(String[] args) {
-		MatrixPuzzle bfs = new MatrixPuzzle();
-		bfs.initGraph();
-		bfs.bfs();
+		MatrixPuzzle puzzle = new MatrixPuzzle();
+		puzzle.initGraph();
+		puzzle.bfs();
 	}
 }
